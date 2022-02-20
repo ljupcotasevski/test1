2cbf06a129c7b00210e325fa7e745d18a50e49df
novat <ljupcotasevski24@gmail.com>
2022-02-20 22:27:33 +0100

    vezba 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Say hello");
+        System.out.println("Ljupco");
+        
+
+
+	// write your code here
+    }
+}
