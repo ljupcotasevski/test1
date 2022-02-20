c02d31d831d42e32dc093d0b6be00f36ed3384da
novat <ljupcotasevski24@gmail.com>
2022-02-20 22:35:00 +0100

    New Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Say hello");
-        System.out.println("Ljupco");
-        
+        System.out.println("Ljupco 2");
+
 
 
 	// write your code here
